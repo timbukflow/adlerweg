fc90beda936d4f2ca36e0f106a15d648df4c0adb
timbuk <ivoschwizer@gmail.com>
2019-04-15 18:26:16 +0200

    miete

---
+++ miete.php
@@ -0,0 +1,347 @@
+<!doctype html>
+<html class="no-js" lang="de-CH">
+
+<head>
+   <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>adlerweg</title>
+    <meta name="robots" content="all">
+    <meta name="robots" content="index, follow">
+    <meta name="author" content="Graf AG Totalunternehmung">
+    
+    <meta name="google-site-verification" content="...folgt..." />
+    
+    <meta property="og:type" content="business.business">
+    <meta property="og:title" content="playstressaway">
+    <meta property="og:url" content="http://adlerweg.ch">
+    <meta property="business:contact_data:street_address" content="Schaffhauserstrasse 115">
+    <meta property="business:contact_data:locality" content="Kloten">
+    <meta property="business:contact_data:region" content="8302">
+    <meta property="business:contact_data:postal_code" content="8302">
+    <meta property="business:contact_data:country_name" content="Switzerland">
+    
+    <meta name="description" content="folgt">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#005a32">
+    <meta name="msapplication-TileColor" content="#fbfbfb">
+    <meta name="theme-color" content="#ffffff">
+
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link href="https://fonts.googleapis.com/css?family=Amiri:400,700" rel="stylesheet">
+</head>
+
+<body>
+  <!--[if IE]>
+    <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+  <![endif]-->
+
+    <nav id="navcontainer" >  
+        <div class="logocontainer">
+            <a href="index" target="_top">
+                <img class="logo" alt="adlerweg-wittenbach-logo" src="img/adlerweg-wittenbach-logo.svg">
+                <h1 class="header">ADLERWEG WITTENBACH</h1>
+            </a>
+            <div id="navburger">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>    
+            </div>
+            <div class="phone">
+                <img alt="adlerweg-wittenbach-logo" src="img/adlerweg-wittenbach-phone.svg">
+            </div>
+            <div class="mail">
+                <img alt="adlerweg-wittenbach-logo" src="img/adlerweg-wittenbach-mail.svg">
+            </div>
+        </div>
+        <div class="nav">
+            <ul>
+                <li><a href="lage">Lage</a></li>
+                <li><a href="grundrisse">Grundrisse</a></li>
+                <li><a href="preise">Preis</a></li>
+                <li><a href="kontakt">Kontakt</a></li>
+                <a class="logo-weiss" href="index" target="_top">
+                    <svg version="1.1" id="adlerlogo" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 303 153.8" style="enable-background:new 0 0 303 153.8;" xml:space="preserve">
+                        <path class="a" d="M151.5,98.1c-48.9-0.2-46.8,27-46.8,27S127.1,140.6,151.5,98.1"/>
+                        <path class="c" d="M151.5,98.1c24.3,42.5,46.8,27,46.8,27S200.4,98,151.5,98.1"/>
+                        <path class="b" d="M151.5,98.1c-24.6,42.3,0,54,0,54S176.1,140.4,151.5,98.1"/>
+                        <path class="st0" d="M151.5,98.1c0,0,50.1,0,75,0c37.5,0,37.5-30,37.5-30"/>
+                        <path class="st0" d="M151.7,2.1c0,0-40.8,39.8-0.3,63C191.9,41.9,151.7,2.1,151.7,2.1z"/>
+                        <path class="st0" d="M151.5,65.1L151.5,65.1c0,0,37.5-27,75-27c24.9,0,75,0,75,0s0,30-37.5,30h-37.5c0,0-12.6,0-37.5,0 c-37.5,0-37.5,30-37.5,30s12.6,0,37.5,0c37.5,0,37.5-30,37.5-30"/>
+                        <path class="st0" d="M39,68.1c0,0,0,30,37.5,30c24.9,0,75,0,75,0"/>
+                        <path class="st0" d="M76.5,68.1c0,0,0,30,37.5,30c24.9,0,37.5,0,37.5,0s0-30-37.5-30C89.1,68.1,76.5,68.1,76.5,68.1H39 c-37.5,0-37.5-30-37.5-30s50.1,0,75,0c37.5,0,75,27,75,27l0,0"/>
+                    </svg>
+                </a>
+            </ul>
+        </div>
+    </nav>
+    
+    <article class="article-miete-container top">
+        <h2>
+            <span>Haus A2</span><span class="pnp"> (Pauschale Nebenkosten, Preise in CHF)</span>  
+        </h2>
+        <div class="article-miete">
+            <table>   
+                <tr>  
+                    <th>Haus</th>
+                    <th>Ebene</th>    
+                    <th>Wohnung</th>  
+                    <th>Fläche</th>
+                    <th class="right">Miete / Monat</th>
+                    <th class="right">Nebenkosten</th>
+                </tr>   
+                <tr>
+                    <td>A2</td>
+                    <td>EG</td> 
+                    <td><span>1.1. / 3,5er</span></td>
+                    <td>89.50 m²</td> 
+                    <td class="right">1’520.00</td>
+                    <td class="right">160.00</td>
+                </tr> 
+                <tr>
+                    <td class="nol"></td>
+                    <td class="nol"></td>
+                    <td><span>1.2. / 4,5er</span></td>
+                    <td>118.00 m²</td> 
+                    <td class="right">1’770.00</td>
+                    <td class="right">210.00</td>
+                </tr>
+                <tr>
+                    <td class="nol"></td>
+                    <td class="nol"></td>
+                    <td><span>1.3. / 2,5er</span></td>
+                    <td>56.30 m²</td> 
+                    <td class="right">1’100.00</td>
+                    <td class="right">110.00</td>
+                </tr>
+                
+                <tr>
+                    <td>A2</td>
+                    <td>1. OG</td> 
+                    <td><span>2.1. / 3,5er</span></td>
+                    <td>89.80 m²</td> 
+                    <td class="right">1’540.00</td>
+                    <td class="right">160.00</td>
+                </tr> 
+                <tr>
+                    <td class="nol"></td>
+                    <td class="nol"></td>
+                    <td><span>2.2. / 2,5er</span></td>
+                    <td>75.00 m²</td> 
+                    <td class="right">1’270.00</td>
+                    <td class="right">130.00</td>
+                </tr>
+                <tr>
+                    <td class="nol"></td>
+                    <td class="nol"></td>
+                    <td><span>2.3. / 4,5er</span></td>
+                    <td>112.90 m²</td> 
+                    <td class="right">1’790.00</td>
+                    <td class="right">210.00</td>
+                </tr>
+                
+                <tr>
+                    <td>A2</td>
+                    <td>2. OG</td> 
+                    <td><span>3.1. / 3,5er</span></td>
+                    <td>89.80 m²</td> 
+                    <td class="right">1’560.00</td>
+                    <td class="right">160.00</td>
+                </tr> 
+                <tr>
+                    <td class="nol"></td>
+                    <td class="nol"></td>
+                    <td><span>3.2. / 2,5er</span></td>
+                    <td>75.00 m²</td> 
+                    <td class="right">1’300.00</td>
+                    <td class="right">130.00</td>
+                </tr>
+                <tr>
+                    <td class="nol"></td>
+                    <td class="nol"></td>
+                    <td><span>3.3. / 4,5er</span></td>
+                    <td>112.90 m²</td> 
+                    <td class="right">1’810.00</td>
+                    <td class="right">210.00</td>
+                </tr>
+                
+                <tr>
+                    <td>A2</td>
+                    <td>3. OG</td> 
+                    <td><i>4.1. / 3,5er</i></td>
+                    <td><i>89.80 m²</i></td> 
+                    <td class="right"><i>reserviert</i></td>
+                    <td class="right"><i>reserviert</i></td>
+                </tr> 
+                <tr>
+                    <td class="nol"></td>
+                    <td class="nol"></td>
+                    <td><span>4.2. / 2,5er</span></td>
+                    <td>75.00 m²</td> 
+                    <td class="right">1’330.00</td>
+                    <td class="right">130.00</td>
+                </tr>
+                <tr>
+                    <td class="nol"></td>
+                    <td class="nol"></td>
+                    <td><span>4.3. / 4,5er</span></td>
+                    <td>112.90 m²</td> 
+                    <td class="right">1’830.00</td>
+                    <td class="right">210.00</td>
+                </tr>
+                
+                <tr>
+                    <td>A2</td>
+                    <td>4. OG</td> 
+                    <td><span>5.1. / 3,5er</span></td>
+                    <td>89.80 m²</td> 
+                    <td class="right">1’610.00</td>
+                    <td class="right">160.00</td>
+                </tr> 
+                <tr>
+                    <td class="nol"></td>
+                    <td class="nol"></td>
+                    <td><span>5.2. / 2,5er</span></td>
+                    <td>75.00 m²</td> 
+                    <td class="right">1’360.00</td>
+                    <td class="right">130.00</td>
+                </tr>
+                <tr>
+                    <td class="nol"></td>
+                    <td class="nol"></td>
+                    <td><span>5.3. / 4,5er</span></td>
+                    <td>112.90 m²</td> 
+                    <td class="right">1’850.00</td>
+                    <td class="right">210.00</td>
+                </tr>
+                
+                <tr>
+                    <td>A2</td>
+                    <td>5. OG</td> 
+                    <td><span>6.1. / 3,5er</span></td>
+                    <td>89.80 m²</td> 
+                    <td class="right">1’690.00</td>
+                    <td class="right">160.00</td>
+                </tr> 
+                <tr>
+                    <td class="nol"></td>
+                    <td class="nol"></td>
+                    <td><span>6.2. / 2,5er</span></td>
+                    <td>75.00 m²</td> 
+                    <td class="right">1’410.00</td>
+                    <td class="right">130.00</td>
+                </tr>
+                <tr>
+                    <td class="nol"></td>
+                    <td class="nol"></td>
+                    <td><i>6.3. / 4,5er</i></td>
+                    <td><i>112.90 m²</i></td> 
+                    <td class="right"><i>reserviert</i></td>
+                    <td class="right"><i>reserviert</i></td>
+                </tr>
+                
+                <tr><td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td class="right"><a href="miete"><span>&#8595; Grundrisse PDF</span></a></td>
+                </tr>
+            </table>
+        </div>
+    </article>
+    
+    <div class="footer">
+        <ul>
+            <li><a href="lage">Lage</a></li>
+            <li><a href="grundrisse">Grundrisse</a></li>
+            <li><a href="preise">Preise</a></li>
+            <li><a href="kontakt">Kontakt</a></li>     
+        </ul>   
+    </div>
+    
+    
+<!--
+    <div class="form">
+        <?php
+        if(isset($_POST['submit'])){
+            $name = htmlspecialchars(stripslashes(trim($_POST['name'])));
+            $vorname = htmlspecialchars(stripslashes(trim($_POST['vorname'])));
+            $subject = htmlspecialchars(stripslashes(trim($_POST['subject'])));
+            $email = htmlspecialchars(stripslashes(trim($_POST['email'])));
+            $message = htmlspecialchars(stripslashes(trim($_POST['message'])));
+            if(!preg_match("/^[A-Za-z .'-]+$/", $name)){
+                $name_error = 'Invalid name';
+            }
+            if(!preg_match("/^[A-Za-z .'-]+$/", $vorname)){
+                $vorname_error = 'Invalid name';
+            }
+            if(!preg_match("/^[A-Za-z .'-]+$/", $subject)){
+                $subject_error = 'Invalid subject';
+            }
+            if(!preg_match("/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/", $email)){
+                $email_error = 'Invalid email';
+            }
+            if(strlen($message) === 0){
+                $message_error = 'Your message should not be empty';
+            }
+        }
+        ?>
+    
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
+            <label for="name">Name:</label><br>
+            <input type="text" name="name">
+            <p><?php if(isset($name_error)) echo $name_error; ?></p>
+            
+            <label for="vorname">Vorname:</label><br>
+            <input type="text" name="vorname">
+            <p><?php if(isset($vorname_error)) echo $vorname_error; ?></p>
+            
+            <label for="subject">Subject:</label><br>
+            <input type="text" name="subject">
+            <p><?php if(isset($subject_error)) echo $subject_error; ?></p>
+            <label for="email">Email:</label><br>
+            <input type="text" name="email">
+            <p><?php if(isset($email_error)) echo $email_error; ?></p>
+            <label for="message">Message:</label><br>
+            <textarea name="message"></textarea>
+            <p><?php if(isset($message_error)) echo $message_error; ?></p>
+            <input type="submit" name="submit" value="Submit">
+            <?php 
+            if(isset($_POST['submit']) && !isset($name_error) && !isset($subject_error) && !isset($email_error) && !isset($message_error)){
+                $to = 'ivoschwizer@gmail.com'; // edit here
+                $body = " Name: $name\n E-mail: $email\n Message:\n $message";
+                if(mail($to, $subject, $body)){
+                    echo '<p style="color: green">Message sent</p>';
+                }else{
+                    echo '<p>Error occurred, please try again later</p>';
+                }
+            }
+            ?>
+        </form>
+    </div>
+    
+    -->
+ 
+    
+  <script src="js/vendor/modernizr-3.7.1.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+  <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.3.1.min.js"><\/script>')</script>
+  <script src="js/plugins.js"></script>
+  <script src="js/main.js"></script>
+
+  <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+  <!--<script>
+    window.ga = function () { ga.q.push(arguments) }; ga.q = []; ga.l = +new Date;
+    ga('create', 'UA-XXXXX-Y', 'auto'); ga('set','transport','beacon'); ga('send', 'pageview')
+  </script>
+  <script src="https://www.google-analytics.com/analytics.js" async defer></script> -->
+</body>
+
+</html>
